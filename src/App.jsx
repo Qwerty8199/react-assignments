50e2177914dda2340001ba64c5c5695f06b3e7d1
qwerty8199 <vinodvin135@gmail.com>
2024-07-31 20:52:00 +0530

    Forgot to push App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Counter } from "./assign1/Counter";
 import { GetData } from "./assign2/GetPublicData";
 import { GetData3 } from "./assign3/GetData";
+import { AnotherComponent } from "./assign4/components/AnotherComponent";
+import { SampleComponent } from "./assign4/components/SampleComponent";
+import { ThemeProvider } from "./assign4/hooks/ThemeProvider";
 
 function App() {
 
   return (
     <>
-      <GetData3 />
+      <ThemeProvider>
+        <SampleComponent />
+        <AnotherComponent />
+      </ThemeProvider>
     </>
   )
 }
