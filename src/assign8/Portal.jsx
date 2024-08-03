cee07c6b16394ccd184795d80bf6a2a4f92d8742
qwerty8199 <vinodvin135@gmail.com>
2024-08-03 19:38:45 +0530

    Modals using React portals- Added CSS

--- src/assign8/Portal.jsx
+++ src/assign8/Portal.jsx
@@ -6,8 +6,8 @@ const Modal = ({ isOpen, onClose, children }) => {
     if (!isOpen) return null;
   
     return ReactDOM.createPortal(
-      <div className="modal">
-        <div className="modal-overlay" >
+      <div className="modal bg-white p-20 rounded-lg shadow-lg flex fixed top-0 left-0 w-full h-max items-center justify-center z-50">
+        <div className="modal-overlay " >
              <button onClick={onClose}> Close </button></div>
         <div className="modal-content">
           {children}
