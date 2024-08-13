a9e9c05d63283ba28e082492d9dc5a1b24c4b3ec
qwerty8199 <vinodvin135@gmail.com>
2024-08-13 13:27:11 +0530

    -

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,16 @@ import { AnotherComponent } from "./assign4/components/AnotherComponent";
 import { SampleComponent } from "./assign4/components/SampleComponent";
 import { ThemeProvider } from "./assign4/hooks/ThemeProvider";
 import { ProfileComponent } from "./assign5/ProfileComponent";
-import { TopPage } from "./assign6/top";
-<<<<<<< HEAD
+import { TopPage } from "./assign6/top";  
 import { Assign7Page } from "./assign7/Assign7Page";
 import { Assign8Page } from "./assign8/Assign8Page";
-=======
 import { Assign9Page } from "./assign9/Assign9Page";
->>>>>>> dev
 
 function App() {
 
   return (
     < >
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <Assign8Page />
-=======
-      <Assign9Page/>
->>>>>>> dev
-=======
-    <ThemeProvider10>
-      <Assign10Page/>
-    </ThemeProvider10>
->>>>>>> dev
+      <Assign10Page />
     </>
   )
 }
